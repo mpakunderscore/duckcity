6d400b7f75c664bcbe0c90f5547f4d5809011535
mpakunderscore <mpakunderscore@gmail.com>
2017-05-28 00:44:55 +0300

    Socket fix

--- web/js/client.js
+++ web/js/client.js
@@ -15,13 +15,15 @@
 
 // let io = require('socket.io')(http);
 
-const ip = 'quackserver.herokuapp.com';
+// const ip = 'quackserver.herokuapp.com';
+
+const ip = 'localhost';
 
 const port = 3000;
 
 // let socket = SocketIOClient('http://' + ip + ':' + port);
 
-let socket = io('https://' + ip + ':' + port);
+let socket = io('wss://' + ip + ':' + port);
 // socket.on('connect', function(){});
 // socket.on('event', function(data){});
 // socket.on('disconnect', function(){});
